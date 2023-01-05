e01267f88db1715c53df42fc935f00cd64ea5e15
Rafael Diniz <rafael@riseup.net>
2023-01-05 19:46:23 +0300

    creating csv correctly

--- grab_and_store.sh
+++ grab_and_store.sh
@@ -6,6 +6,7 @@ echo "timestamp, latitude, longitude, Vbatt, Vcurr, SOC" > ${OUTPUT_FILE}
 
 while true; do
     gpspipe -w -n 10 | grep -m 1 TPV | jq -j '[.time, .lat, .lon] | @csv' >> ${OUTPUT_FILE}
+    echo -n "," >> ${OUTPUT_FILE}
     ./get_battery_data.py >> ${OUTPUT_FILE}
     echo >> ${OUTPUT_FILE}
 done
