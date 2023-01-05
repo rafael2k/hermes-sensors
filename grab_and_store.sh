e26a919669dbc1b357b6bb09cbcb4ce67b8c912a
Rafael Diniz <rafael@riseup.net>
2023-01-05 21:05:22 +0300

    adapt bash script to use the python script

--- grab_and_store.sh
+++ grab_and_store.sh
@@ -2,13 +2,16 @@
 
 OUTPUT_FILE="${1:-output.csv}"
 
-echo "timestamp, latitude, longitude, Vbatt, Vcurr, SOC" > ${OUTPUT_FILE}
+# echo "timestamp, latitude, longitude, Vbatt, Vcurr, SOC" > ${OUTPUT_FILE}
 
 # TODO: use a timer!
 
-while true; do
-    gpspipe -w -n 10 | grep -m 1 TPV | jq -j '[.time, .lat, .lon] | @csv' >> ${OUTPUT_FILE}
-    echo -n "," >> ${OUTPUT_FILE}
-    ./get_battery_data.py >> ${OUTPUT_FILE}
-    echo >> ${OUTPUT_FILE}
-done
+./get_sensors_data.py > ${OUTPUT_FILE}
+
+#while true; do
+#
+#    gpspipe -w -n 10 | grep -m 1 TPV | jq -j '[.time, .lat, .lon] | @csv' >> ${OUTPUT_FILE}
+#    echo -n "," >> ${OUTPUT_FILE}
+#    ./get_battery_data.py >> ${OUTPUT_FILE}
+#    echo >> ${OUTPUT_FILE}
+#done
