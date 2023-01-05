8d3121b43203be2d3b54f792763d95a5a11f3813
Rafael Diniz <rafael@riseup.net>
2023-01-05 20:30:27 +0300

    all in python now...

--- get_battery_data.py
+++ get_battery_data.py
@@ -11,19 +11,20 @@ agps_thread = AGPS3mechanism()  # Instantiate AGPS3 Mechanisms
 agps_thread.stream_data()  # From localhost (), or other hosts, by example, (host='gps.ddns.net')
 agps_thread.run_thread()  # Throttle time to sleep after an empty lookup, default 0.2 second, default daemon=True
 
+sleep(1)
+
+print('timestamp, latitude, longitude, Vbatt, Vcurr, SOC')
+
 while True:
-    print('---------------------')
-    print(                   agps_thread.data_stream.time)
-    print('Lat:{}   '.format(agps_thread.data_stream.lat))
-    print('Lon:{}   '.format(agps_thread.data_stream.lon))
-    print('Speed:{} '.format(agps_thread.data_stream.speed))
-    print('Course:{}'.format(agps_thread.data_stream.track))
-    print('---------------------')
-    vbatt = controller.get_battery_voltage()
-    cbatt = controller.get_battery_current()
-    soc = controller.get_battery_state_of_charge()
-    print(str(vbatt) + ',' + str(cbatt) + ',' + str(soc), end ='')
-    sleep(1)
+    print(agps_thread.data_stream.time + ',', end='')
+    print(agps_thread.data_stream.lat + ',', end='')
+    print(agps_thread.data_stream.lon + ',', end='')
+#    print(agps_thread.data_stream.speed
+#    print(agps_thread.data_stream.track)
+    print(controller.get_battery_voltage() + ',', end='')
+    print(controller.get_battery_current() + ',', end='')
+    print(controller.get_battery_state_of_charge())
+    sleep(0.9)
 
 # controller.get_solar_voltage
 # controller.get_load_voltage
