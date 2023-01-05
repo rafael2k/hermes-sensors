b68278b7f782ea5f0fa42bfdea088af57aea77dc
Rafael Diniz <rafael@riseup.net>
2023-01-05 21:36:31 +0300

    experimenting with timers

--- get_sensors_data2.py
+++ get_sensors_data2.py
@@ -18,15 +18,46 @@ print('timestamp, latitude, longitude, Vbatt, Abatt, SOC')
 
 stopped = Event()
 
-def loop():
-    while not stopped.wait(interval): # the first call is in `interval` secs
-        print(agps_thread.data_stream.time + ',', end='')
-        print(str(agps_thread.data_stream.lat) + ',', end='')
-        print(str(agps_thread.data_stream.lon) + ',', end='')
-#       print(agps_thread.data_stream.speed
-#       print(agps_thread.data_stream.track)
-        print(str(controller.get_battery_voltage()) + ',', end='')
-        print(str(controller.get_battery_current()) + ',', end='')
-        print(str(controller.get_battery_state_of_charge()))
+def sensors():
+    print(agps_thread.data_stream.time + ',', end='')
+    print(str(agps_thread.data_stream.lat) + ',', end='')
+    print(str(agps_thread.data_stream.lon) + ',', end='')
+    print(str(controller.get_battery_voltage()) + ',', end='')
+    print(str(controller.get_battery_current()) + ',', end='')
+    print(str(controller.get_battery_state_of_charge()))
 
-Thread(target=loop).start()
+#           print(agps_thread.data_stream.speed
+#            print(agps_thread.data_stream.track)
+
+class RepeatedTimer:
+
+    """Repeat `function` every `interval` seconds."""
+
+    def __init__(self, interval, function):
+        self.interval = interval
+        self.function = function
+        self.start = time.time()
+        self.event = Event()
+        self.thread = Thread(target=self._target)
+        self.thread.start()
+
+    def _target(self):
+        while not self.event.wait(self._time):
+            self.function()
+
+    @property
+    def _time(self):
+        return self.interval - ((time.time() - self.start) % self.interval)
+
+    def stop(self):
+        self.event.set()
+        self.thread.join()
+
+
+# start timer
+timer = RepeatedTimer(1, sensors)
+
+sleep(10)
+
+# stop timer
+timer.stop()
