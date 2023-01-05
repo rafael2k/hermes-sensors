005e0a1c10b78712ba3a92034fac7ed08f171dba
Rafael Diniz <rafael@riseup.net>
2023-01-05 19:44:15 +0300

    improved sample script

--- grab_and_store.sh
+++ grab_and_store.sh
@@ -2,7 +2,10 @@
 
 OUTPUT_FILE="${1:-output.csv}"
 
+echo "timestamp, latitude, longitude, Vbatt, Vcurr, SOC" > ${OUTPUT_FILE}
+
 while true; do
     gpspipe -w -n 10 | grep -m 1 TPV | jq -j '[.time, .lat, .lon] | @csv' >> ${OUTPUT_FILE}
+    ./get_battery_data.py >> ${OUTPUT_FILE}
     echo >> ${OUTPUT_FILE}
 done
