f02299dc64db8f689eb455871e689a6afccaafbf
Rafael Diniz <rafael@riseup.net>
2023-01-05 18:52:09 +0300

    make it work

--- grab_and_store.sh
+++ grab_and_store.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-OUTPUT_FILE="${$1:-output.csv}"
+OUTPUT_FILE="${1:-output.csv}"
 
 while true; do
-    gpspipe -w -n 10 | grep -m 1 TPV | jq -r '[.time, .lat, .lon] | @csv' >> ${OUTPUT_FILE}
+    gpspipe -w -n 10 | grep -m 1 TPV | jq -j '[.time, .lat, .lon] | @csv' >> ${OUTPUT_FILE}
+    echo >> ${OUTPUT_FILE}
 done
