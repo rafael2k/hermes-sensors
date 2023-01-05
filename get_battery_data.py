a93d885109a0b07b7ab3897077547c29b9376557
Rafael Diniz <rafael@riseup.net>
2023-01-05 19:38:02 +0300

    get battery data works

--- get_battery_data.py
+++ get_battery_data.py
@@ -5,13 +5,13 @@ from epevermodbus.driver import EpeverChargeController
 
 controller = EpeverChargeController("/dev/ttyUSB1", 1)
 
-vbatt = controller.get_solar_voltage()
-cbatt = controller.get_battery_current
-soc = controller.get_battery_state_of_charge
-print(vbatt + ',' + cbatt + ',' + soc)
+vbatt = controller.controller.get_battery_voltage()
+cbatt = controller.get_battery_current()
+soc = controller.get_battery_state_of_charge()
+print(str(vbatt) + ',' + str(cbatt) + ',' + str(soc), end ='')
 
+# controller.get_solar_voltage
 # controller.get_load_voltage
-# controller.get_battery_voltage
-
-# controller.get_load_current
+#
 # controller.get_solar_current
+# controller.get_load_current
